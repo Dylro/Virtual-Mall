9a1109da3566b762a7ee9f90f4f7179fe7df86f8
Dylro <96742256+Dylro@users.noreply.github.com>
2022-01-14 22:18:17 +0530

    Add files via upload

---
+++ Exit.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Exit
+{
+    public void ty()
+    {
+        System.out.println('\u000c');//to clear window for next screen
+        System.out.println("*********Thank you for using McKINLEY MALL services***********");
+        System.out.println("~~~~~~~~~~~~~~~~~~Please visit us again~~~~~~~~~~~~~");
+        
+    }
+}
